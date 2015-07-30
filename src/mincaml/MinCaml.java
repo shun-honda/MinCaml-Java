660f6cacf6493d28c2bb211635e77980340c677f
shun-honda <shun.honda.suffix@gmail.com>
2015-07-30 14:46:55 +0900

    introduce mincaml parse(shell mode)

--- src/mincaml/MinCaml.java
+++ src/mincaml/MinCaml.java
@@ -3,6 +3,7 @@ package mincaml;
 import java.io.IOException;
 
 import nez.NezOption;
+import nez.SourceContext;
 import nez.lang.Grammar;
 import nez.lang.GrammarFile;
 import nez.util.ConsoleUtils;
@@ -18,10 +19,13 @@ public class MinCaml {
 	public final static String License = "BSD-License Open Source";
 
 	private static Grammar grammar;
+	private static MinCamlTreeTransducer treeTransducer;
 
 	public static final void main(String[] args) {
 		displayVersion();
 		grammar = newMinCamlGrammar();
+		treeTransducer = new MinCamlTreeTransducer();
+		shell();
 	}
 
 	public final static void displayVersion() {
@@ -35,12 +39,49 @@ public class MinCaml {
 	public final static Grammar newMinCamlGrammar() {
 		if(mincamlGrammar == null) {
 			try {
-				mincamlGrammar = GrammarFile.loadGrammarFile("mincaml.nez", NezOption.newDefaultOption());
+				mincamlGrammar = GrammarFile.loadGrammarFile("grammar/mincaml.nez", NezOption.newDefaultOption());
 			} catch (IOException e) {
 				ConsoleUtils.exit(1, "can't load mincaml.nez");
 			}
 		}
 		return mincamlGrammar.newGrammar("File");
+	}
+
+	public final static MinCamlTree parse(String urn, int linenum, String text) {
+		SourceContext source = SourceContext.newStringSourceContext(urn, linenum, text);
+		MinCamlTree node = (MinCamlTree) grammar.parse(source, treeTransducer);
+		if(node == null) {
+			ConsoleUtils.println(source.getSyntaxErrorMessage());
+		}
+		System.out.println("parsed:\n" + node + "\n");
+		return node;
+	}
+
+	private static void shell() {
+		int linenum = 1;
+		String command = null;
+		while((command = readLine()) != null) {
+			parse("<stdio>", linenum, command);
+			linenum += (command.split("\n").length);
+		}
+	}
+
+	private static String readLine() {
+		ConsoleUtils.println("\n>>>");
+		Object console = ConsoleUtils.getConsoleReader();
+		StringBuilder sb = new StringBuilder();
+		while(true) {
+			String line = ConsoleUtils.readSingleLine(console, "   ");
+			if(line == null) {
+				return null;
+			}
+			if(line.equals("")) {
+				return sb.toString();
+			}
+			ConsoleUtils.addHistory(console, line);
+			sb.append(line);
+			sb.append("\n");
+		}
 	}
 
 }
