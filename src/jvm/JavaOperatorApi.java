f9348c3ae9952f4b059698c7a7c148ce26e4b40d
shun-honda <shun.honda.suffix@gmail.com>
2015-08-02 23:28:45 +0900

    added Java oerator api

---
+++ src/jvm/JavaOperatorApi.java
@@ -0,0 +1,109 @@
+package jvm;
+
+public class JavaOperatorApi {
+
+	public static int Add(int x, int y) {
+		return x + y;
+	}
+
+	public static int Sub(int x, int y) {
+		return x - y;
+	}
+
+	public static int Mul(int x, int y) {
+		return x * y;
+	}
+
+	public static int Div(int x, int y) {
+		return x / y;
+	}
+
+	public static double FAdd(double x, double y) {
+		return x + y;
+	}
+
+	public static double FSub(double x, double y) {
+		return x - y;
+	}
+
+	public static double FMul(double x, double y) {
+		return x * y;
+	}
+
+	public static double FDiv(double x, double y) {
+		return x / y;
+	}
+
+	public static boolean LessThan(double x, double y) {
+		return x < y;
+	}
+
+	public static boolean LessThanEquals(double x, double y) {
+		return x <= y;
+	}
+
+	public static boolean GreaterThan(double x, double y) {
+		return x > y;
+	}
+
+	public static boolean GreaterThanEquals(double x, double y) {
+		return x >= y;
+	}
+
+	public static boolean Equals(double x, double y) {
+		return x == y;
+	}
+
+	public static boolean NotEquals(double x, double y) {
+		return x != y;
+	}
+
+	public static boolean LessThan(int x, int y) {
+		return x < y;
+	}
+
+	public static boolean LessThanEquals(int x, int y) {
+		return x <= y;
+	}
+
+	public static boolean GreaterThan(int x, int y) {
+		return x > y;
+	}
+
+	public static boolean GreaterThanEquals(int x, int y) {
+		return x >= y;
+	}
+
+	public static boolean Equals(int x, int y) {
+		return x == y;
+	}
+
+	public static boolean NotEquals(int x, int y) {
+		return x != y;
+	}
+
+	public static boolean LessThan(boolean x, boolean y) {
+		return false;
+	}
+
+	public static boolean LessThanEquals(boolean x, boolean y) {
+		return false;
+	}
+
+	public static boolean GreaterThan(boolean x, boolean y) {
+		return false;
+	}
+
+	public static boolean GreaterThanEquals(boolean x, boolean y) {
+		return false;
+	}
+
+	public static boolean Equals(boolean x, boolean y) {
+		return x == y;
+	}
+
+	public static boolean NotEquals(boolean x, boolean y) {
+		return x != y;
+	}
+
+}
